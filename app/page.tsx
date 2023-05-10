c29e2e07931cdc87c4e0d87372c00dd8c00b8263
alirmemon37 <alirmemona@gmail.com>
2023-05-10 12:50:43 +0530

    added About and Contact pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import Footer from "@/components/General/Footer";
-import Navbar from "@/components/General/Navbar";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
