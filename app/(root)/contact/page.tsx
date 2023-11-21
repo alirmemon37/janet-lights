ae44cd8ae2be1caabf3eb66c0ca1da5e5c0757da
alirmemon37 <alirmemona@gmail.com>
2023-11-22 01:03:50 +0530

    (chore): Update contact page UI

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -5,6 +5,8 @@ import FormTextarea from "@/components/Create/FormTextarea";
 import apiEndpoint from "@/utils/apiEndpoint";
 import React, { useState } from "react";
 import { ContactFormData } from "@/types/types";
+import Image from "next/image";
+import Link from "next/link";
 
 const Contact = () => {
   const [formData, setFormData] = useState<ContactFormData>(
@@ -38,26 +40,37 @@ const Contact = () => {
 
   return (
     <section className="w-full max-w-full flex-start flex-col">
-      <div className="contact_form">
-        <div className="md:col-span-2 p-6 md:px-12 md:pt-8 md:pb-12 lg:px-16 lg:pt-10 lg:pb-16">
-          <h2 className="mt-5 text-4xl font-bold leading-[1.15] text-white sm:text-5xl">
-            Contact Us Today
-          </h2>
-          <p className="mt-5 text-lg sm:text-xl max-w-2xl text-white">
-            Click the button below to chat with us on WhatsApp or fill the form and we will get back to you as soon as possible.
-          </p>
+      <h2 className="mt-5 head_text blue_gradient">Contact Us Today</h2>
+      <p className="mt-5 text-lg sm:text-xl max-w-2xl text-white desc">
+        Click the button below to chat with us on WhatsApp or fill the form and
+        we will get back to you as soon as possible.
+      </p>
+      <div className="contact_form mt-10">
+        <div className="md:col-span-2 p-6 md:pb-12 lg:px-10 lg:pb-16">
           <div>
-            <a href="https://wa.me/" target="_blank" className="mt-5 inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-600">
-              <img src="/assets/icons/whatsapp.svg" alt="whatsapp" className="w-6 h-6 mr-2" />
+            <Link
+              href="https://wa.me/"
+              target="_blank"
+              className="mt-5 inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md green_gradient_btn"
+            >
+              <Image
+                src="/assets/icons/whatsapp.svg"
+                alt="whatsapp"
+                width={24}
+                height={24}
+                className="w-6 h-6 mr-2"
+              />
               Chat with us on WhatsApp
-            </a>
-          <div></div>
+            </Link>
           </div>
-          <form className="grid grid-cols-1 md:grid-cols-2 mt-10 w-full gap-x-5 gap-y-8 contact-form" onSubmit={handleSubmit}>
+          <form
+            className="grid grid-cols-1 md:grid-cols-2 mt-10 w-full gap-x-5 gap-y-8 contact-form"
+            onSubmit={handleSubmit}
+          >
             <div className="flex flex-col gap-2">
               <label
                 htmlFor="name"
-                className="font-satoshi font-semibold text-md text-gray-700"
+                className="font-satoshi font-semibold text-lg text-gray-700"
               >
                 Full Name
               </label>
@@ -69,13 +82,12 @@ const Contact = () => {
                 value={formData?.name || ""}
                 placeholder="John Smith"
                 required
-                className="w-full p-4 border-none bg-[#fdf2e9] rounded-[9px] focus:ring-8 focus:ring-yellow-500 focus:outline-none"
               />
             </div>
             <div className="flex flex-col gap-2">
               <label
                 htmlFor="phone"
-                className="font-satoshi font-semibold text-md text-gray-700"
+                className="font-satoshi font-semibold text-lg text-gray-700"
               >
                 Phone Number
               </label>
@@ -88,13 +100,12 @@ const Contact = () => {
                 placeholder="9999999999"
                 pattern="[0-9]{10}"
                 required
-                className="w-full p-4 border-none bg-[#fdf2e9] rounded-[9px] focus:ring-8 focus:ring-yellow-500 focus:outline-none"
               />
             </div>
             <div className="flex flex-col gap-2">
               <label
                 htmlFor="email"
-                className="font-satoshi font-semibold text-md text-gray-700"
+                className="font-satoshi font-semibold text-lg text-gray-700"
               >
                 Email
               </label>
@@ -106,13 +117,12 @@ const Contact = () => {
                 value={formData?.email || ""}
                 placeholder="johnsmith@gmail.com"
                 required
-                className="w-full p-4 border-none bg-[#fdf2e9] rounded-[9px] focus:ring-8 focus:ring-yellow-500 focus:outline-none"
               />
             </div>
             <div className="flex flex-col gap-2 md:col-span-2">
               <label
                 htmlFor="message"
-                className="font-satoshi font-semibold text-md text-gray-700"
+                className="font-satoshi font-semibold text-lg text-gray-700"
               >
                 Message
               </label>
@@ -120,15 +130,13 @@ const Contact = () => {
                 name="message"
                 value={formData?.message || ""}
                 onChange={handleContactDataChange}
-                className="w-full p-4 border-none bg-[#fdf2e9] rounded-[9px] focus:ring-8 focus:ring-yellow-500 focus:outline-none resize-none md:max-w-full"
+                className="w-full p-4 resize-none md:max-w-full"
                 placeholder="Hi, I would like to know more about your products."
                 rows={5}
                 required
               />
             </div>
-            <button className="contact_form_button">
-              Contact Us
-            </button>
+            <button className="blue_gradient_btn">Contact Us</button>
           </form>
         </div>
         <div className="contact_form_img"></div>
